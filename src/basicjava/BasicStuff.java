83412119bf7cff4f8bb79e3c3a239af81b16e8ba
Augusta <africke@macalester.edu>
2019-09-09 13:51:03 -0500

    finished hw0

---
+++ src/basicjava/BasicStuff.java
@@ -0,0 +1,32 @@
+package basicjava;
+
+public class BasicStuff {
+    public static void main(String[] args) {
+////        int age0 = 19;
+//        double age0 = 19.2;
+//
+////        int age1 = 34;
+//        double age1 = 34.4;
+//
+//        System.out.println("We are " + age0 + " and " + age1 + " years old!");
+//
+//        double sumOfAges = (age0 + age1);
+//        System.out.println("The sum of our ages is " + sumOfAges +"!");
+//
+//        System.out.println("The sum of our ages is " + (age0 + age1) + "!");
+//        //if a programmer does not include parentheses around the ints then the computer
+//        //will get confused and think that they are strings
+
+        System.out.println(6/3);
+        System.out.println(6/4); //outputs 1 because they are both ints, change to double
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
+
+
+
+
+
+
+    }
+}
